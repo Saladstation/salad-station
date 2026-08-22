9e0bf1171afa3f0263ef9ba6911970c25ee5834c
Saladstation <saladstationdoubt@duck.com>
2026-08-22 15:26:28 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 function umwandeln() {
-    document.getElementById("ergebnis").textContent = "ES FUNKTIONIERT!";
+    let text = document.getElementById("eingabe").value.toLowerCase();
+
+    // Doppelbuchstaben schützen
+    text = text.replaceAll("ss", "ß");
+    text = text.replaceAll("bb", "¤");
+    text = text.replaceAll("nn", "§");
+
+    const kombinationen = {
+        "sch": "Ś",
+        "ung": "Ŋ",
+        "ver": "V",
+        "st": "Š",
+        "er": "Ə",
+        "ge": "G",
+        "en": "Ɛ",
+        "ei": "Æ",
+        "ie": "Ï",
+        "ch": "Ç",
+        "ck": "K",
+        "be": "B",
+        "au": "A"
+    };
+
+    // Längere Kombinationen zuerst
+    const sortierteKombinationen = Object.entries(kombinationen)
+        .sort((a, b) => b[0].length - a[0].length);
+
+    for (const [suchtext, ersatz] of sortierteKombinationen) {
+        text = text.split(suchtext).join(ersatz);
+    }
+
+    // Geschützte Doppelbuchstaben wiederherstellen
+    text = text.replaceAll("ß", "ss");
+    text = text.replaceAll("¤", "bb");
+    text = text.replaceAll("§", "nn");
+
+    // Ergebnis anzeigen
+    document.getElementById("ergebnis").textContent = text;
 }
