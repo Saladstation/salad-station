0a1c3edb233625c72424873460b1e43b9b597ca7
Saladstation <saladstationdoubt@duck.com>
2026-08-22 15:16:49 +0200

    script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 function umwandeln() {
-  let text = document.getElementById("eingabe").value.toLowerCase();
+    let text = document.getElementById("eingabe").value.toLowerCase();
 
-// Doppelbuchstaben schützen
-text = text.replaceAll("ss", "ß");
-text = text.replaceAll("bb", "¤");
-text = text.replaceAll("nn", "§");
+    // Doppelbuchstaben schützen
+    text = text.replaceAll("ss", "ß");
+    text = text.replaceAll("bb", "¤");
+    text = text.replaceAll("nn", "§");
 
-  const kombinationen = {
-    "sch": "Ś",
-    "ung": "Ŋ",
-    "ver": "V",
-    "st": "Š",
-    "er": "Ə",
-    "ge": "G",
-    "en": "Ɛ",
-    "ei": "Æ",
-    "ie": "Ï",
-    "ch": "Ç",
-    "ck": "K",
-    "be": "B",
-    "au": "A",
-  };
+    const kombinationen = {
+        "sch": "Ś",
+        "ung": "Ŋ",
+        "ver": "V",
+        "st": "Š",
+        "er": "Ə",
+        "ge": "G",
+        "en": "Ɛ",
+        "ei": "Æ",
+        "ie": "Ï",
+        "ch": "Ç",
+        "ck": "K",
+        "be": "B",
+        "au": "A"
+    };
 
-  // Längere Kombinationen haben Vorrang
-  const sortierteKombinationen = Object.entries(kombinationen)
-    .sort((a, b) => b[0].length - a[0].length);
+    // Längere Kombinationen haben Vorrang
+    const sortierteKombinationen = Object.entries(kombinationen)
+        .sort((a, b) => b[0].length - a[0].length);
 
-  for (const [suchtext, ersatz] of sortierteKombinationen) {
-    text = text.split(suchtext).join(ersatz);
-  }
-// Geschützte Doppelbuchstaben wiederherstellen
-text = text.replaceAll("ß", "ss");
-text = text.replaceAll("¤", "bb");
-text = text.replaceAll("§", "nn");
+    for (const [suchtext, ersatz] of sortierteKombinationen) {
+        text = text.split(suchtext).join(ersatz);
+    }
 
-  document.getElementById("ausgabe").value = text;
+    // Geschützte Doppelbuchstaben wiederherstellen
+    text = text.replaceAll("ß", "ss");
+    text = text.replaceAll("¤", "bb");
+    text = text.replaceAll("§", "nn");
+
+    // Ergebnis anzeigen
+    document.getElementById("ergebnis").textContent = text;
 }
